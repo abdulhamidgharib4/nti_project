1353fbbff4442fc63cacbca40952af04eadedf15
abdulhamidgharib4 <abdulhamidgharib4@gmail.com>
2026-09-04 05:13:33 +0300

    feature 1 Done

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import random
+# 1 - with re module 
+import re 
+
 
 def remove_same_digits(num):
     if num[0] == num[1] or num[0] == num[2] or num[0] == num[3]:
@@ -10,6 +13,36 @@ def remove_same_digits(num):
     
     return False
 
+def game_culc(guess_1,guess_2):
+    stars = 0
+    points = 0
+    if guess_1[0]==guess_2[0]:
+        stars+=1
+    if guess_1[1]==guess_2[1]:
+        stars+=1
+    if guess_1[2]==guess_2[2]:
+        stars+=1
+    if guess_1[3]==guess_2[3]:
+        stars+=1
+    if guess_1[0]==guess_2[1] or guess_1[0]==guess_2[2] or guess_1[0]==guess_2[3]:
+        points+=1
+    if guess_1[1]==guess_2[0] or guess_1[1]==guess_2[2] or guess_1[1]==guess_2[3]:
+        points+=1
+    if guess_1[2]==guess_2[0] or guess_1[2]==guess_2[1] or guess_1[2]==guess_2[3]:
+        points+=1
+    if guess_1[3]==guess_2[0] or guess_1[3]==guess_2[1] or guess_1[3]==guess_2[2]:
+        points+=1
+    return stars,points
+
+
+def get_guess():
+    while True:
+        player_guess = input("Enter your guess number : ")
+        if re.fullmatch(r'\d{4}',player_guess) and not  remove_same_digits(player_guess):
+            return str(player_guess)
+        else:
+            print("Please try agine ! ")
+
 
 
 while True:
@@ -20,22 +53,29 @@ while True:
     if check_1==False and check_2==False:
         break
 
-print("Player 1 number:", player_1)
-print("Player 2 number:", player_2)
+# print("player 1 your number is : ",player_1)
+# print("player 2 your number is : ",player_2)
+
+while True:
+    print("player 1 your ruond:")
+    player_1_guess = get_guess()
+    stars,points= game_culc(player_1_guess,player_1)
+    if stars==4:
+        print("player 1 win the game !")
+        break
+    print(f"\nplayer 1 your guess is {player_1_guess} and the result is {stars} stars and {points} points\n")
+    print("player 2 your ruond:")
+    player_2_guess = get_guess()
+    stars,points= game_culc(player_2_guess,player_2)
+    if stars==4:
+        print("player 2 win the game !")
+        break
+    print(f"\nplayer 2 your guess is {player_2_guess} and the result is {stars} stars and {points} points\n")
+    
 
 
 
-# 1 - with re module 
-import re 
-def get_guess():
-    while True:
-        player_guess = input(" Enter your guess number : ")
-        if re.fullmatch(r'\d{4}',player_guess) and not  remove_same_digits(player_guess):
-            return int(player_guess)
-        else:
-            print("Please try agine ! ")
 
-guess_number = get_guess()
 
 
 
