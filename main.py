045b15ab960decb02768e70110cf2d71d91dd4b1
seba amer <sebaamer29@icloud.com>
2026-09-06 02:52:42 +0300

    updates

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-import random
-# 1 - with re module 
-import re 
-
-from tools import ts 
-
-
-
-def get_guess():
-    while True:
-        player_guess = input("Enter your guess number : ")
-        if re.fullmatch(r'\d{4}',player_guess) and not ts.remove_same_digits(player_guess):
-            return str(player_guess)
-        else:
-            print("Please try agine ! ")
-
 
 
 
